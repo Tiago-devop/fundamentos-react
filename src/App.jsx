41d2e0aef086eb1f5ec9702258f67855d1db1f71
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-11-27 11:44:54 -0300

    add estilo nos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 
 import Card from "./components/layout/Card";
@@ -7,19 +8,21 @@ import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
 
 export default (_) => (
-  <div id="app">
+  <div className="App">
     <h1>Fundamentos React</h1>
-    <Card titulo="#04 - Desafio Aleatório">
-      <Aleatorio min={1} max={100} />
-    </Card>
-    <Card titulo="#03 - Fragmento">
-      <Fragmento />
-    </Card>
-    <Card titulo="#02 - Com Parâmetro">
-      <ComParametro titulo="Situação do Aluno" aluno="Juca Sipó" nota={9.3} />
-    </Card>
-    <Card titulo="#01 - Fragmento">
-      <Primeiro />
-    </Card>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={1} max={100} />
+      </Card>
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro titulo="Situação do Aluno" aluno="Juca Sipó" nota={9.3} />
+      </Card>
+      <Card titulo="#01 - Fragmento">
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
