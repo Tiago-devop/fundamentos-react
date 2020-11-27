274a36153333c8389693d8d96161c502b8405a1d
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-11-27 12:28:04 -0300

    adcionando style no componente

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,33 @@ import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
 
 export default (_) => (
+
   <div className="App">
+
     <h1>Fundamentos React</h1>
+
     <div className="Cards">
-      <Card titulo="#04 - Desafio Aleatório">
+
+      <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
         <Aleatorio min={1} max={100} />
       </Card>
-      <Card titulo="#03 - Fragmento">
+
+      <Card titulo="#03 - Fragmento" color="#E94C6F">
         <Fragmento />
       </Card>
-      <Card titulo="#02 - Com Parâmetro">
-        <ComParametro titulo="Situação do Aluno" aluno="Juca Sipó" nota={9.3} />
+
+      <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+        <ComParametro 
+          titulo="Situação do Aluno" 
+          aluno="Juca Sipó" 
+          nota={9.3} 
+        />
       </Card>
-      <Card titulo="#01 - Fragmento">
+
+      <Card titulo="#01 - Fragmento" color="#588C73">
         <Primeiro />
       </Card>
+
     </div>
   </div>
 );
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,12 +2,15 @@ import "./Card.css";
 import React from "react";
 
 export default (props) => {
+  const cardStyled = {
+    backgroundColor: props.color || "#F00",
+    borderColor: props.color || "#F00",
+  };
+
   return (
-    <div className="Card">
+    <div className="Card" style={cardStyled}>
       <div className="Title">{props.titulo}</div>
-      <div className="Content">
-        {props.children}
-      </div>
+      <div className="Content">{props.children}</div>
     </div>
   );
 };
